d9b7cb886941ee7b01545ea79790df844990c730
Hubert Słomion <hslomion@gmail.com>
2019-05-21 02:17:43 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -1,20 +1,44 @@
 const app = new Vue({
     el: '#app',
     data: {
-        code: null,
+        code: '',
+        codes: '',
+        id: ''
+    },
+    created: function () {
+        axios.get('https://jsonplaceholder.typicode.com/todos/5')
+            .then(function (response) {
+                this.codes = response.data
+                console.log(codes);
+                this.id = this.codes.id;
+                console.log(id);
+                {
+                    console.log(id);
+                }
+                if(id == 5)
+
+                console.log('equal')
+                else
+                console.log('nie')
+            }
+            )
+            .catch(function (error) {
+                this.codes = "Zly kod"
+            })
     },
     methods: {
         createPost() {
             axios.post('http://rest.learncode.academy/api/fotobudka/code', {
                 code: this.code
             },
-                {        headers: {
+                {
+                    headers: {
                         'Content-type': 'application/json',
                     }
-            }).then((response) => { })
+                }).then((response) => { })
                 .catch((e) => {
                     console.error(e)
                 })
-        }
+            }
     }
 });
