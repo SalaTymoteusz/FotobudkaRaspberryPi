28491ffe3c8628183367d264ed6665aa01778f85
tomiJ <tomi.jozefczak@gmail.com>
2019-05-28 09:18:52 +0200

    catalogs

--- uploadPhoto.php
+++ uploadPhoto.php
@@ -12,19 +12,26 @@ if($mysql->connect_error){
     $response["MESSAGE"] = "SERVER ERROR";
     $response["STATUS"] = 500;
 }else{
-	echo 'poloczono z baza';
-    if (is_uploaded_file($_FILES['photo']['tmp_name']) && $_POST['code']){
+	echo 'polaczono z baza';
+    if (is_uploaded_file($_FILES['photo']['tmp_name']) && $_POST['code'] && $_POST['catalog_id']){
+        echo 'zuploadowano plik';
         $tmp_file = $_FILES['photo']['tmp_name'];
         $photo_name = $_FILES['photo']['name'];
-        $upload_dir= "./photos/" . $photo_name;
-        $sql = "INSERT INTO Photos (name, code) VALUES ('{$photo_name}', {$_POST['code']})";
+        $catalog_id = $_POST['catalog_id'];
+        $upload_dir= "./photos/" . $catalog_id . "/" . $photo_name;
+         
+        $sql = "INSERT INTO Photos (name, code, catalog_id) VALUES ('{$photo_name}', {$_POST['code']}, {$_POST['catalog_id']})";
+        echo $sql;
         if(move_uploaded_file($tmp_file, $upload_dir) && $mysql->query($sql)){
             $response['MESSAGE'] = 'UPLOAD SUCCED';
             $response['STATUS'] = 200;
+
         }else{
             $response['MESSAGE'] = 'UPLOAD FAILED';
             $response['STATUS'] = 404;
+
         }
+
     }else{
         $response['MESSAGE'] = 'INVALID REQUEST';
         $response['STATUS'] = 400;
@@ -32,5 +39,6 @@ if($mysql->connect_error){
 }
 
 echo json_encode($response);
+echo 'test';
 
 ?>
